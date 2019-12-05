aa45f93a613c5cebcc07f7238af4226375e3ab07
Amaresh <biplab@iotegral.in>
2019-12-05 19:56:32 +0530

    update chart

--- pages/home/home.controller.js
+++ pages/home/home.controller.js
@@ -5,8 +5,8 @@
         .module('app')
         .controller('HomeController', HomeController);
 
-    HomeController.$inject = ['UserService', '$rootScope'];
-    function HomeController(UserService, $rootScope) {
+    HomeController.$inject = ['UserService', '$rootScope','$scope'];
+    function HomeController(UserService, $rootScope,$scope) {
         var vm = this;
 
         vm.user = null;
@@ -40,6 +40,42 @@
                 loadAllUsers();
             });
         }
+
+
+
+
+
+        $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [
+          [65, 59, 80, 81, 56, 55, 40],
+          [28, 48, 40, 19, 86, 27, 90]
+        ];
+        $scope.onClick = function (points, evt) {
+          console.log(points, evt);
+        };
+        $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+        $scope.options = {
+          scales: {
+            yAxes: [
+              {
+                id: 'y-axis-1',
+                type: 'linear',
+                display: true,
+                position: 'left'
+              },
+              {
+                id: 'y-axis-2',
+                type: 'linear',
+                display: true,
+                position: 'right'
+              }
+            ]
+          }
+        };
+
+
+
     }
 
 })();
