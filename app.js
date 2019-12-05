3291de820dbee24c8efe8c2a6f4a4ecc1b70daef
Amaresh <biplab@iotegral.in>
2019-12-05 16:02:02 +0530

    update charts

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
     'use strict';
 
     angular
-        .module('app', ['ngRoute', 'ngCookies','toastr', 'ngAnimate'])
+        .module('app', ['ngRoute', 'ngCookies','toastr', 'ngAnimate','chart.js'])
         .config(config)
         .run(run);
 
